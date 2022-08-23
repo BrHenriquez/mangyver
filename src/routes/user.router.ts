f824d32e5acc03f9d304207f8e73e44df8e20d16
Jeffrei <hjefrei@gmail.com>
2022-08-23 10:54:52 -0400

    feature: user endpoint filter

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -5,7 +5,10 @@ import passwordValidator from "password-validator";
 import { log } from "../config/logger";
 import jwt_decode from "jwt-decode";
 import { getUser } from "../repositories/user.repository";
-import { getUserByIdValidator, getUsersValidator } from "./validators/user/fetch-users.validator";
+import {
+  getUserByIdValidator,
+  getUsersValidator,
+} from "./validators/user/fetch-users.validator";
 import { updateUserAuthValidator } from "./validators/user/update-user-status.validator";
 
 const router = express.Router();
@@ -57,7 +60,13 @@ router.get("/", [getUsersValidator], async (req: Request, res: Response) => {
   const response = await controller.getUsers(
     profile?.operation.id,
     Number(req.query?.skip),
-    Number(req.query?.take)
+    Number(req.query?.take),
+    <string>req.query?.name,
+    <string>req.query?.email,
+    <string>req.query?.SAPCode,
+    <string>req.query?.SAPUser,
+    <string>req.query?.areaId,
+    <string>req.query?.lineId
   );
   const results = JSON.parse(JSON.stringify(response));
   results.data.map((result: any) => {
@@ -80,29 +89,37 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:id", [getUserByIdValidator], async (req: Request, res: Response) => {
-  const controller = new UserController();
-  const response = await controller.getUser(req.params.id);
-  if (!response) {
-    log.warn(response);
-    return res.status(404).send({ message: "No User found" });
+router.get(
+  "/:id",
+  [getUserByIdValidator],
+  async (req: Request, res: Response) => {
+    const controller = new UserController();
+    const response = await controller.getUser(req.params.id);
+    if (!response) {
+      log.warn(response);
+      return res.status(404).send({ message: "No User found" });
+    }
+    log.silly(response);
+    return res.send(response);
   }
-  log.silly(response);
-  return res.send(response);
-});
+);
 
-router.put("/:id", [updateUserAuthValidator], async (req: Request, res: Response) => {
-  const controller = new UserController();
-  const response = await controller.updateUserStatus(
-    req.params.id,
-    req.query.auth
-  );
-  if (!response) {
-    log.warn(response);
-    return res.status(404).send({ message: "No User found" });
+router.put(
+  "/:id",
+  [updateUserAuthValidator],
+  async (req: Request, res: Response) => {
+    const controller = new UserController();
+    const response = await controller.updateUserStatus(
+      req.params.id,
+      req.query.auth
+    );
+    if (!response) {
+      log.warn(response);
+      return res.status(404).send({ message: "No User found" });
+    }
+    log.silly(response);
+    return res.send(response);
   }
-  log.silly(response);
-  return res.send(response);
-});
+);
 
 export default router;
